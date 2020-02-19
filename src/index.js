67b9b6714c167294b8e91244e11904bca2dff44f
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-02-19 16:37:09 -0700

    idk

--- src/index.js
+++ src/index.js
@@ -65,6 +65,21 @@ class Board extends React.Component {
     );
   }
 }
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -83,21 +98,6 @@ function calculateWinner(squares) {
     }
   }
   return null;
-}
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
 }
 // ========================================
 ReactDOM.render(
